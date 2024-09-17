3bfa5a11def0586bae01fb70cf92a75f168cc53e
ToinADMIX <toin.admix151@gmail.com>
2024-09-18 00:32:16 +0900

    Add files via upload

---
+++ ADMIXHP/script.js
@@ -0,0 +1,16 @@
+function langja(){
+	home1h1.innerHTML = "Toin ADMIX";
+	home1p.innerHTML = "Toin ADMIXは、RCJ サッカーライトウェイト競技に参加しているチームです。<br>目標は、世界大会で1位を取ることです。";
+	home2h1.innerHTML = "私たちは誰？";
+	home2p.innerHTML = "Toin ADMIXは3人の高校生で構成されています。";
+	home3h1.innerHTML = "私たちは何をしている？";
+	home3p.innerHTML = "私たちは役割を設計、実装、プログラミングに分担しています。";
+}
+function langen(){
+	home1h1.innerHTML = "Toin ADMIX";
+	home1p.innerHTML = "Toin ADMIX is a team which is participating in<br>RCJ Soccer Lightweight league.<br>Its goal is to get the 1st prize in the world cup.";
+	home2h1.innerHTML = "Who Are We ?";
+	home2p.innerHTML = "3 high school students are consisting Toin ADMIX.";
+	home3h1.innerHTML = "What Do We Do ?";
+	home3p.innerHTML = "We divide roles to designing, implemantation, and programming.";
+}
